c0c5386579c46303c836629fdd3c0d54f9f7c7cf
Simon Guillorit <simon.guillorit@gmail.com>
2017-08-07 14:32:59 +0200

    Implemented coach answer

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def answer
+    if params[:question] == "I am going to work right now!"
+      exit
+    elsif params[:question][-1, 1] == "?"
+      @answer =  "Silly question, get dressed and go to work!"
+    else
+      @answer =  "I don\'t care, get dressed and go to work!"
+    end
+  end
+
+  def ask
+  end
+end
